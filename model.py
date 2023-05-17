35c07c92caf9251b048411bc22324cfc69ce07f8
guavacoderepo <guavacodeplus@gmail.com>
2023-05-17 22:55:36 +0100

    latest

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 df = pd.read_csv("dataset.csv")
 
-df = df.drop(['index'], axis=1)
+# df = df.drop(['index'], axis=1)
 # df = df.drop(['doc_index'], axis=1)
 
 
@@ -13,9 +13,32 @@ df = df.drop(['index'], axis=1)
 sns.set(style='whitegrid')
 # print(df.head)
 # sns.barplot(df)
-sns.scatterplot(df)
+# sns.scatterplot(df)
+# sns.countplot(data=df, y="china", x="index")
 # sns.countplot(x=df["scale-free"], )
 # sns.countplot(x=df["china"])
+
+# plt.scatter(x=df["index"], y=df["machine learning"])
+# plt.scatter(x=df["index"], y=df["covid"])
+# plt.scatter(x=df["index"], y=df["scale-free"])
+# plt.scatter(x = df["index"], y=df["china"])
+
+# plt.legend(["machile learning", "covid 19", "scale-free", "china"])
+
+
+# plt.scatter(x=df["index"], y=df["machine learning"], )
+
+
+# plt.scatter(x=df["index"], y=df["covid"])
+# plt.scatter(x=df["index"], y=df["scale-free"])
+plt.scatter(x=df["index"], y=df["china"])
+
+plt.grid(color='green', linewidth=0.5)
+
+plt.xlabel("Document index")
+plt.ylabel("China")
+
+plt.show()
 
 
 # plt.hist(df)
